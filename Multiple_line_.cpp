0fb9ca39d2d9bece5c1fd92c3fc88474fb700463
tblevin <tblevin2@email.tjc.edu>
2024-08-29 10:17:15 -0500

    Add files via upload

--- Multiple_line_.cpp
+++ Multiple_line_.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-int main()
+int ()
 {
 	cout << "I first surmised the Horses' Heads Were toward Eternity. (Emily Dickinson)" << endl;
 	cout << "Identity theft is not a joke, Jim! Millions of families suffer every year. (The Office U.S)" << endl;
